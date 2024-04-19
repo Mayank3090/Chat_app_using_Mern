afee3b9fee421863c3e1aa6cdfdec2ea129f9cec
Mayank3090 <mayankjoshi3090@gmail.com>
2024-04-19 20:45:08 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
@@ -13,6 +14,7 @@ import {app,server} from './socket/socket.js';
 
 const PORT=process.env.port||5051;
 
+const __dirname=path.resolve();
 dotenv.config();
 
 app.use(express.json());
@@ -22,6 +24,11 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+   res.sendFile(path.join(__dirname,"fronend","dist","index.html"))
+})
 //app.get("/",(req,res)=>{
    // res.send("hello world!!");
 //})
