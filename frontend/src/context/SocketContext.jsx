3ea02ceff4a063ac77ce6a81ab3ad208aa716b11
Mayank3090 <mayankjoshi3090@gmail.com>
2024-04-19 21:52:41 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5051", {
+			const socket = io("https://chat-app-using-mern-rqpp.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
